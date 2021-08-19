77b14b2d5464f4bde1d63f0e2697ca78ca7d0ac2
Himanshu Shrotriya <80567079+Himanshu15shotriya@users.noreply.github.com>
2021-08-19 12:46:07 +0530

    Update adminController.js

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -224,7 +224,7 @@ module.exports.addBook = async (req, res) => {
 // @access   PRIVATE
 module.exports.updateBook = async(req, res) => {
     try {
-        if((req.body.bookquantity).length != 0 ){
+        if((req.body.bookquantity).length != 0 && req.body.bookquantity >= 0 ){
             const bookUpdate = {}
             if(req.body.bookquantity) bookUpdate.bookquantity = req.body.bookquantity;
         
@@ -244,7 +244,7 @@ module.exports.updateBook = async(req, res) => {
                 })
             }
         }else{
-            res.status(500).send({success: false, message: "Please Enter Book Quantity"})
+            res.status(500).send({success: false, message: "Please Enter Valid Quantity"})
         }
     } catch (error) {
         res.status(500).send({success: false, message:error.message})
@@ -472,7 +472,7 @@ module.exports.userDetails = async(req, res) => {
 // @access   PRIVATE
 module.exports.userCardUpdate = async(req,res) => {
     try{        
-        if((req.body.card).length > 0){
+        if((req.body.card).length > 0 && req.body.card >= 0){
             const cardUpdate = {}
             if(req.body.card) cardUpdate.card = req.body.card
             const user = await User.findOneAndUpdate(
@@ -495,7 +495,7 @@ module.exports.userCardUpdate = async(req,res) => {
         }else{
             res.status(400).json({
                 success : false ,
-                message :"Please enter the value"
+                message :"Please enter valid value"
             })
         }
     }
